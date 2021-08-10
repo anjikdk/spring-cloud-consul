79fb9a27ffaf793922cbaf8e3c2370dea882abbe
Ramanjaneyulu Dayyala <anjikdk@gmail.com>
2021-08-10 19:06:19 +0530

    Initial Commit

---
+++ spring-cloud-consul-student/src/test/java/com/example/springcloudconsulstudent/SpringCloudConsulStudentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcloudconsulstudent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConsulStudentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
